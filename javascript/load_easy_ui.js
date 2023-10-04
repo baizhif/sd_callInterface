f63b0dbc36f0d1d76ace0aabd9df3d31880359b2
baizhif <135608974+baizhif@users.noreply.github.com>
2023-10-05 00:11:33 +0800

    Update load_easy_ui.js

--- javascript/load_easy_ui.js
+++ javascript/load_easy_ui.js
@@ -1,5 +1,5 @@
 setTimeout(function() {
-    const callInterface = document.getElementById("callInterface");
+    const callInterface = document.getElementById("tab_callInterface");
     const callInterface_div = document.createElement("div");
     callInterface_div.innerHTML = '<iframe id="callInterfaceIframe" name="Frame" src="/callInterface/easy_ui.html" style="width: 100%; height: 100%;"></iframe>';
     callInterface.appendChild(callInterface_div);
